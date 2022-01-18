b62417578cc88872aa849363b578413509718d77
bruno.faustino <bruno.faustino@cognitivo.ai>
2022-01-17 22:11:14 -0300

    This will drop rows in the dataset that are not in the proper geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -33,6 +33,11 @@ def go(args):
     # Drop outliers
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
+
+    # This will drop rows in the dataset that are not in the proper geolocation.
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
     df.to_csv("clean_sample.csv", index=False)
